db66a9cba60110266bcbfbd6649a816467a65128
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2025-04-11 20:59:15 +0530

    updateted Project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Navbar from './component/navbar';
+import About from './pages/about';
+import Contact from './pages/contact';
 import Footer from './component/footer';
+
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
